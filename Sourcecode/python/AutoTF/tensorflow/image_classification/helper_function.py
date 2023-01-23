196202f2e4536717f8b73ca81de0b4a428a362b3
paweenp <paween.pongsomboon@gmail.com>
2023-01-23 22:00:25 +0100

    add tf img classification to branch

---
+++ Sourcecode/python/AutoTF/tensorflow/image_classification/helper_function.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+original_image_cache = {}
+
+def preprocess_image(image):
+  
+  image = np.array(image)
+  # reshape into shape [batch_size, height, width, num_channels]
+  img_reshaped = tf.reshape(image, [1, image.shape[0], image.shape[1], image.shape[2]])
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  image = tf.image.convert_image_dtype(img_reshaped, tf.float32)
+  return image
+
+def load_image_from_local(img_path, image_size=256, dynamic_size=False, max_dynamic_size=512 ):
+    """Returns an image with shape [1, height, width, num_channels]."""
+    img = Image.open(img_path)
+    img = preprocess_image(img)
+	
+ 	# Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+    img_raw = img	
+    if tf.reduce_max(img) > 1.0:
+        img = img / 255.
+    if len(img.shape) == 3:
+        img = tf.stack([img, img, img], axis=-1)
+    if not dynamic_size:
+        img = tf.image.resize_with_pad(img, image_size, image_size)
+    elif img.shape[1] > max_dynamic_size or img.shape[2] > max_dynamic_size:
+        img = tf.image.resize_with_pad(img, max_dynamic_size, max_dynamic_size)
+    return img, img_raw
+
+def show_image(image, title=''):
+  image_size = image.shape[1]
+  w = (image_size * 6) // 320
+  plt.figure(figsize=(w, w))
+  plt.imshow(image[0], aspect='equal')
+  plt.axis('off')
+  plt.title(title)
+  plt.show()
