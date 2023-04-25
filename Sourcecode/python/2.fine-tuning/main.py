c6814ba551ae9dc5e626b48740dfcc0fade3ef8e
paweenp <paween.pongsomboon@gmail.com>
2023-04-25 15:50:04 +0200

    add fine-tune alexnet to branch

---
+++ Sourcecode/python/2.fine-tuning/main.py
@@ -0,0 +1,196 @@
+# Pytorch fine-tuning with AlexNet
+
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+input_size = 224
+batch_size = 32
+num_epochs = 1
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'validation']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model logits and calculate loss
+                    logits = model(inputs)
+                    loss = criterion(logits, labels)
+
+                    _, preds = torch.max(logits, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'validation' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'validation':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+def initialize_model(num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model = None
+    input_size = 0
+
+    model = models.alexnet(weights='IMAGENET1K_V1')
+    set_parameter_requires_grad(model, feature_extract)
+    num_ftrs = model.classifier[6].in_features
+    model.classifier[6] = nn.Linear(num_ftrs,num_classes)
+
+    return model
+
+
+
+
+## Main script start here 
+def main():
+    print("Initializing Datasets and Dataloaders...")
+
+    data_dir = '../../../../Dataset/flowers/'
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'validation': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                            data_transforms[x])
+                    for x in ['train', 'validation']}
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+                                                shuffle=True, num_workers=4)
+                for x in ['train', 'validation']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'validation']}
+
+    class_names = image_datasets['train'].classes
+    num_classes = len(class_names)
+
+    # Initialize the model for this run
+    # define pretrained model
+    alexnet = models.alexnet()
+
+    model = initialize_model(num_classes, feature_extract, use_pretrained=True)
+    model.classifier[6] = nn.Linear(4096,num_classes)
+    model = model.to(device)
+
+    # Print the model we just instantiated
+    print(model)
+
+    params_to_update = model.parameters()
+    print("Params to learn: ")
+
+    if feature_extract:
+        params_to_update = []
+        for name,param in model.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        for name,param in model.named_parameters():
+            if param.requires_grad == True:
+                print("\t",name)
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+    # Setup the loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # Train and evaluate
+    model, hist = train_model(model, dataloaders_dict,
+                            criterion, optimizer_ft,
+                            num_epochs=num_epochs)
+
+    #   Save the network checkpoint and optimizer
+    print("Save model ...")
+    state = {
+        'model': model.state_dict(),
+        'optim' : optimizer_ft,        
+    }
+    torch.save(state, './models/alenet_finetune.pth')
+    
+if __name__ == '__main__':
+    main()
+    print("End of Script")
